15a48b9f13a257b40576a0a757a4775aefd6be94
PRADDIP5 <praddip05@gmail.com>
2024-09-17 14:49:13 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,7 +1,7 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
-TEST(StringCalculatorTestSuite,add_InputStringIsEmpty_ExpectedIsZero) { // Testsuite name, Test case name
+TEST(StringCalculatorTestSuite,add_InputStringIsEmpty_ExpectedOutputIsZero) { // Testsuite name, Test case name
   // Arrrange
   string input = "";
   int expectedValue = 0;
@@ -11,12 +11,34 @@ TEST(StringCalculatorTestSuite,add_InputStringIsEmpty_ExpectedIsZero) { // Tests
   ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(StringCalculatorTestSuite,add_InputStringIsZero_ExpectedIsZero) {
+TEST(StringCalculatorTestSuite,add_InputStringIsZero_ExpectedOutputIsZero) {
 // Arrrange
   string input = "0";
   int expectedValue = 0;
   //Act
   int actualValue = Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
+
+TEST(StringCalculatorTestSuite,add_InputStringIsOne_ExpectedOutputIsOne) {
+// Arrrange
+  string input = "1";
+  int expectedValue = 1;
+  //Act
+  int actualValue = Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
+
+TEST(StringCalculatorTestSuite,add_InputTwoNumbersOneDelimeter_ExpectedIsSumofTwoNumbers) {
+// Arrrange
+  string first_input,second_input;
+  first_input = "1";
+  second_input = "2";
+  int expectedValue = 3;
+  //Act
+  int actualValue = Add(first_input,second_input);
   //Assert
   ASSERT_EQ(actualValue,expectedValue);
 }
