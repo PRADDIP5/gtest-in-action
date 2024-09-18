488ef728115a48671b13097396056885852e5732
PRADDIP5 <praddip05@gmail.com>
2024-09-18 10:58:56 +0530

    Update ValueParameterization.Test.cpp

--- ValueParameterization.Test.cpp
+++ ValueParameterization.Test.cpp
@@ -21,7 +21,7 @@ INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFi
   
 ));
 
-TEST_P(StringCalculatorParameterFixture,ParameterizedTest){
+TEST_F(StringCalculatorParameterFixture,ParameterizedTest){
       input= std::get<0>(GetParam());
       expectedValue= std::get<1>(GetParam());
       actualValue=Add(input);
